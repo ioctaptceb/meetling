8ef47343c8984c3d04a39e02704c1bfeacee6b56
ioctaptceb <yulia.startsev@gmail.com>
2015-08-12 18:12:06 +0200

    add authentication service js

---
+++ src/services/authentication.service.js
@@ -0,0 +1,46 @@
+function AuthenticationService() {
+  let service = {};
+
+  service.login = login;
+  service.setCredentials = setCredentials;
+  service.clearCredentials = clearCredentials;
+
+  return service;
+
+  function login(username, password, callback) {
+    $timeout(function () {
+      let response;
+      UserService.getByUserName(username)
+        .then(function(user) {
+          if (user !== null && user.password === password) {
+            response = { success: true };
+          } else {
+            response = {success: false, message: 'Username or Password is incorrect' };
+          }
+          callback(response);
+        });
+    }, 1000);
+  }
+
+  function setCredentials(username, password) {
+    // TODO: add Base64 encoding here
+    let authdata = `${username}:${password}`;
+
+    $rootScope.globals = {
+      currentUser: {
+        username: username,
+        authdata: authdata
+      }
+    };
+
+    $http.defaults.headers.common.Authorization = 'Basic ' + authdata;
+    $cookieStore.put('globals', $rootScope.globals);
+  }
+
+  function clearCredentials() {
+    $rootScope.globals = {};
+    $cookieStore.remove('globals');
+    $http.defaults.headers.common.Authorization = 'Basic ';
+  }
+}
+
